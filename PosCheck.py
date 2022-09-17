66ef90206a3de34ff97caa6952c7c67eaf2af0c0
Jan Focke <janfocke12@outlook.de>
2022-09-17 20:40:41 +0200

    Added Function Tests

--- PosCheck.py
+++ PosCheck.py
@@ -3,4 +3,12 @@ def PosCheck(x):
                 return True
         elif x < 0:
                 return False
+        else:
+                return "Neutral"
+
+print ("-10 is Positive: " + str(PosCheck(-10)))
+print ("0 is Positive: " + str(PosCheck(0)))
+print ("5 is Positive: " + str(PosCheck(5)))
+print ("20 is Positive: " + str(PosCheck(20)))
+
 
