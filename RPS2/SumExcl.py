6df0801f22d78fd6cbb3fd69b8f3db93c4ea64b3
Endergamer1 <janfocke12@outlook.de>
2022-11-03 09:35:02 +0100

    Added Function/See Comment

--- RPS2/SumExcl.py
+++ RPS2/SumExcl.py
@@ -2,3 +2,15 @@
 #n sums from 0 to n and is positional only
 #ints excluded is keyword only and not necessary
 
+def SumExcl(n: int, excluded: int =()) -> int:
+        x: range = range(0, n + 1)
+        sum: int = 0
+        for b in x:
+                if b in excluded:
+                        pass
+                else:
+                        sum = sum + b
+        return sum
+
+Test = SumExcl(7, (1,2,4,7))
+print(Test)
