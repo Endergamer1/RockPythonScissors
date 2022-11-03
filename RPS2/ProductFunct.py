f7a08067a231fefaed498a60b9a6f9d2570b5b62
Endergamer1 <janfocke12@outlook.de>
2022-11-03 09:16:28 +0100

    Added Functions to ProductFunct.Py

--- RPS2/ProductFunct.py
+++ RPS2/ProductFunct.py
@@ -1,3 +1,22 @@
 #Write a function that takes one int n and multiplies all integers in the range(1, n + 1)
 #Write another function that takes an argument of a sequence type and returns product of all int in the list.
 
+def ProductN(n: int) -> int:
+        x = range(1, n + 1)
+        result: int = 1
+        for b in x:
+                result *= b
+        return result
+
+def ProductSeq(n: int) -> int:
+        result: int = 1
+        for b in n:
+                result *= b
+        return result
+
+Test1 = ProductN(20)
+print(Test1)
+
+Test2 = ProductSeq((1,4,6,1,8,223,7))
+print(Test2)
+                
